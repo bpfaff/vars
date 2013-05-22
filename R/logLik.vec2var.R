1e780b2e0c5a6198207776dce9a2ad75aaa8f411
Bernhard Pfaff <bernhard@pfaffikus.de>
2013-05-22 21:41:12 +0000

    Added 'df' and 'nobs' attribute to returned object.

--- R/logLik.vec2var.R
+++ R/logLik.vec2var.R
@@ -6,5 +6,8 @@ logLik.vec2var <- function(object, ...){
     r <- -(obs * K/2) * log(2 * pi) - (obs/2) * log(det(Sigma)) - 
         (1/2) * sum(diag(resids %*% solve(Sigma) %*% t(resids)))
     class(r) <- "logLik"
+    params <- length(unlist(object$A)) + length(object$deterministic)
+    attr(r, "df") <- params
+    attr(r, "nobs") <- object$obs
     return(r)
 }
