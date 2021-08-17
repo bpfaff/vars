844273164fd75c1256061d50f4dd3277d8365374
Slava <vlyubchich@users.noreply.github.com>
2021-08-17 16:32:58 -0400

    add lag.restrict option to VARselect

--- R/VARselect.R
+++ R/VARselect.R
@@ -1,6 +1,9 @@
-"VARselect" <-
-function (y, lag.max = 10, type = c("const", "trend", "both",
-    "none"), season = NULL, exogen = NULL)
+"VARselect" <- function(y,
+                        lag.max = 10,
+                        lag.restrict = 0,
+                        type = c("const", "trend", "both", "none"),
+                        season = NULL,
+                        exogen = NULL)
 {
     y <- as.matrix(y)
     if (any(is.na(y)))
@@ -8,6 +11,12 @@ function (y, lag.max = 10, type = c("const", "trend", "both",
     colnames(y) <- make.names(colnames(y))
     K <- ncol(y)
     lag.max <- abs(as.integer(lag.max))
+    #VL: check that lag.restrict is an N0 number below lag.max [0,1,...,lag.max)
+    lag.restrict <- abs(as.integer(lag.restrict))
+    if (lag.restrict >= lag.max) {
+      warning("lag.restrict >= lag.max. Using lag.restrict = 0 instead.")
+      lag.restrict <- 0
+    }
     type <- match.arg(type)
     lag <- abs(as.integer(lag.max + 1))
     ylagged <- embed(y, lag)[, -c(1:K)]
@@ -45,20 +54,34 @@ function (y, lag.max = 10, type = c("const", "trend", "both",
     } else {
       detint <- 0
     }
-    criteria <- matrix(NA, nrow = 4, ncol = lag.max)
+    criteria <- matrix(NA, nrow = 4, ncol = lag.max - lag.restrict)
     rownames(criteria) <- c("AIC(n)", "HQ(n)", "SC(n)", "FPE(n)")
-    colnames(criteria) <- paste(seq(1:lag.max))
-    for (i in 1:lag.max) {
+    colnames(criteria) <- (lag.restrict + 1):lag.max
+    ii <- 1
+    for (i in (lag.restrict + 1):lag.max) {
+      if (lag.restrict == 0) {
         ys.lagged <- cbind(ylagged[, c(1:idx[i])], rhs)
-        sampletot <- nrow(y)
         nstar <- ncol(ys.lagged)
         resids <- lm.fit(x=ys.lagged, y=yendog)$residuals
+      } else {
+        resids <- numeric()
+        for (k in 1:K) {
+          #VL: restrict to 0 coeffs 1,...,lag.restrict for all variables except k-th
+          tmp <- rep(TRUE, K)
+          tmp[k] <- FALSE #do not restrict for the current variable
+          irestr <- which(rep(tmp, lag.restrict))
+          ys.lagged <- cbind(ylagged[, c(1:idx[i])[-irestr]], rhs)
+          resids <- cbind(resids, lm.fit(x = ys.lagged, y = yendog[,k])$residuals)
+        }
+        nstar <- ncol(ys.lagged)
+      }
         sigma.det <- det(crossprod(resids)/sample)
-        criteria[1, i] <- log(sigma.det) + (2/sample) * (i * K^2 + K * detint)
-        criteria[2, i] <- log(sigma.det) + (2 * log(log(sample))/sample) * (i * K^2 + K * detint)
-        criteria[3, i] <- log(sigma.det) + (log(sample)/sample) * (i * K^2 + K * detint)
-        criteria[4, i] <- ((sample + nstar)/(sample - nstar))^K * sigma.det
+        criteria[1, ii] <- log(sigma.det) + (2/sample) * (i * K^2 + K * detint)
+        criteria[2, ii] <- log(sigma.det) + (2 * log(log(sample))/sample) * (i * K^2 + K * detint)
+        criteria[3, ii] <- log(sigma.det) + (log(sample)/sample) * (i * K^2 + K * detint)
+        criteria[4, ii] <- ((sample + nstar)/(sample - nstar))^K * sigma.det
+        ii <- ii + 1
     }
-    order <- apply(criteria, 1, which.min)
-    return(list(selection = order, criteria = criteria))
+    order <- apply(criteria, 1, which.min) + lag.restrict
+    list(selection = order, criteria = criteria)
 }
